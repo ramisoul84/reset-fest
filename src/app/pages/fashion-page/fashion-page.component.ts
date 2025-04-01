e40520263381e52d85265329d7fb90e406802e54
Rami <rami.suliman@mail.ru>
2025-04-01 21:28:52 +0300

    mobile

--- src/app/pages/fashion-page/fashion-page.component.ts
+++ src/app/pages/fashion-page/fashion-page.component.ts
@@ -9,59 +9,39 @@ import { DJ } from '../../_models/dj';
   styleUrl: './fashion-page.component.css'
 })
 export class FashionPageComponent {
-  select:DJ={name:"",info:""};
-   djs: DJ[] = [
-      {name:"Frankey & Sandrino",info:"The music at RE:SET is a journey – one that mirrors the island’s energy, from the sun-soaked afternoons to the starry, rhythmic nights. Our soundscape weaves together groovy house beats, deep, hypnotic techno, and soulful minimal rhythms, all designed to carry you deeper into the moment",img:"assets/images/djs/1.jpeg",expanded:false},
-      {name:"Luca Olivotto",info:"German DJ....   House music specialist",img:"assets/images/djs/2.jpeg",expanded:false},
-      {name:"Eva Crystaltips",info:"The music at RE:SET is a journey – one that mirrors the island’s energy, from the sun-soaked afternoons to the starry, rhythmic nights. Our soundscape weaves together groovy house beats, deep, hypnotic techno, and soulful minimal rhythms, all designed to carry you deeper into the moment",expanded:false},
-      {name:"Quadrakey",info:"",expanded:false},
-      {name:"Nephews",info:"German DJ....   House music specialist",expanded:false},
-      {name:"Siggatunez",info:"",expanded:false},
-      {name:"Sarah Wild",info:"",expanded:false},
-      {name:"Carmen Electro",info:"",expanded:false},
-      {name:"Sam Shure",info:"",expanded:false},
-      {name:"Skatman",info:"",expanded:false},
-      {name:"Glia",info:""},
-      {name:"BRTINZZ",info:""},
-      {name:"Gil bert",info:""},
-      {name:"Blackbox",info:""},
-      {name:"Bianca Banks",info:""},
-      {name:"Tofu und Acid",info:""},
-      {name:"JSAVANT",info:""}, 
-      {name:"Polytox",info:""}, 
-      {name:"Illinios",info:""}, 
-      {name:"Sarica",info:""},
-      {name:"Leandro",info:""}, 
-      {name:"Genji Yoshiba",info:""}, 
-      {name:"Zare",info:""}, 
-      {name:"Slevin Flakes",info:""},
-      {name:"Love Operator",info:""}, 
-      {name:"Toast Hawaii",info:""}, 
-      {name:"Gonzalo Lavin",info:""}, 
-      {name:"Zuleta M",info:""}, 
-      {name:"Dash",info:""}, 
-      {name:"Besch",info:""},
-      {name:"Hanafia",info:""},
-      {name:"Luke Lash",info:""},
-      {name:"A FRIEND",info:""},
-      {name:"Rio",info:""},
-      {name:"Alex Grebe",info:""}, 
-      {name:"DJ Sunburn",info:""},
-      {name:"Dolo Desir",info:""},
-      {name:"Mono Rome",info:""},
-      {name:"Lukas & Lisa",info:""},
-      {name:"Yassine Karoui",info:""},
-      {name:"Selim X Noor Arjoun",info:""}
-    ]
-  
-
   show:boolean=false;
+  src:string= '';
+  
+  fashion: string[] = [
+    'assets/images/fashion/1.jpg',
+    'assets/images/fashion/2.jpg',
+    'assets/images/fashion/3.jpg',
+    'assets/images/fashion/4.jpg',
+    'assets/images/fashion/5.jpg',
+    'assets/images/fashion/6.jpg',
+    'assets/images/fashion/7.jpg',
+    'assets/images/fashion/8.jpg',
+    'assets/images/fashion/9.jpg',
+    'assets/images/fashion/10.jpg',
+    'assets/images/fashion/11.jpg',
+    'assets/images/fashion/12.jpg',
+    'assets/images/fashion/13.jpg',
+    'assets/images/fashion/14.jpg',
+    'assets/images/fashion/15.jpg',
+    'assets/images/fashion/16.jpg',
+    'assets/images/fashion/17.jpg',
+    'assets/images/fashion/18.jpg',
+    'assets/images/fashion/19.jpg',
+    'assets/images/fashion/20.jpg',
+    'assets/images/fashion/21.jpg',
+    'assets/images/fashion/22.jpg',
+    'assets/images/fashion/23.jpg',
+  ];
 
-  toggleSticky(clickedDj: DJ) {
-    const wrapper = document.querySelector('.lineup');
-    const info = document.querySelector('.info');
-    wrapper!.classList.add('move-up');
-    info!.classList.add('show');
-    this.select=clickedDj;
+
+  onImageClick(event: Event):void{
+    const imageElement = event.target as HTMLImageElement;
+    this.src = imageElement.src;
+    this.show = !this.show
   }
 }
