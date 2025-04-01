9323c450403cab3772d98401b0818592c778bca7
Rami <rami.suliman@mail.ru>
2025-04-02 00:23:05 +0300

    footer

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { AnimationOptions, LottieComponent } from 'ngx-lottie';
 
 @Component({
@@ -9,6 +9,7 @@ import { AnimationOptions, LottieComponent } from 'ngx-lottie';
   styleUrl: './hero.component.css'
 })
 export class HeroComponent {
+  @ViewChild('videoHero') videoHero!: ElementRef<HTMLVideoElement>;
   play:boolean=false;
   resetHeroOptions: AnimationOptions = {
     path: 'assets/animations/reset-hero.json',
@@ -17,6 +18,29 @@ export class HeroComponent {
 
   playPause(){
     this.play = !this.play;
-    console.log("play")
   }
+
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const hero= document.getElementById('hero-img');
+    const heroPosition = hero!.getBoundingClientRect().top;
+    if (heroPosition < 0 && !this.play) {
+      setTimeout(() => {
+        this.play = true;
+      }, 5000); 
+   
+    } 
+
+}
+
+/*
+playVideo() {
+  const video = this.videoHero.nativeElement;
+  setTimeout(() => {
+    this.play = true;
+    video.play();
+  }, 5000); 
+}
+*/
 }
